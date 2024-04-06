d079184af77d80c13e86ad583dc5ad6b88cf7c8e
zahgenya <zahgenya@gmail.com>
2024-04-06 18:04:18 +0300

    removed front dir

--- frontend/jest.config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  transform: {
-    "^.+\\.test\\.tsx?$": "ts-jest"
-  },
-  transformIgnorePatterns: ['<rootDir>/node_modules/'],
-}
